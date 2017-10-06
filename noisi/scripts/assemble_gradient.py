262cd0588a7fec14d1e590aee16400ad2bb63190
lermert <laura.ermert@erdw.ethz.ch>
2017-10-06 11:10:12 +0200

    gradient?

--- noisi/scripts/assemble_gradient.py
+++ noisi/scripts/assemble_gradient.py
@@ -58,6 +58,9 @@ def assemble_ascent_dir(source_model,step,snr_min,n_min,save_all=False):
 		cnt_overlap = 0
 		cnt_unavail = 0
 		n = len(data)
+		print('Nr Measurements:')
+		print(n)
+		print('*'*16)
 
 		for i in range(n):
 
@@ -145,38 +148,38 @@ def assemble_ascent_dir(source_model,step,snr_min,n_min,save_all=False):
 			
 	# output metadata
 		
-		with open(outfile,'a') as fh:
-
-			# print info
-			fh.write('Analyzed %g station pairs of %g successfully.\n' %(cnt_success,n))
-			fh.write('No data found for %g station pairs.\n' %cnt_unavail)
-			fh.write('No measurement taken for %g station pairs due to short interstation distance.\n' %cnt_overlap) 
-			fh.write('Signal to noise ratio below threshold for %g station pairs.\n' %cnt_lowsnr)
-			fh.write('Number of staacked windows below threshold for %g station pairs.\n' %cnt_lown)
-			fh.write('\nParameters:==============================================================\n')
-			fh.write('Source dir: %s \n' %source_model)
-			fh.write('Step: %g' %int(step))
-			fh.write('Minimum SNR: %g' %snr_min)
-			fh.write('Minimum stack length: %g' %int(n_min))
-			fh.write('Save all interstation gradients: %s' %str(save_all))
-			fh.write('\n=========================================================================\n')
-			fh.write('Project:\n')
-			# append configurations
-			cfg = open(os.path.join(source_config['project_path'],'config.json')).read()
-			fh.write(cfg)
-			fh.write('\n=========================================================================\n')
-			fh.write('Source model:\n')
-			fh.write(json.dumps(source_config))
-			fh.write('\n=========================================================================\n')
-			fh.write('Measurement:\n')
-			cfg = open(os.path.join(source_config['source_path'],'measr_config.json')).read()
-			fh.write(cfg)
-		
-
-		
-	# collect
-		gradient[ix_basis,:] += kernel * var_weights[ix_basis]
-		del kernel
+			with open(outfile,'a') as fh:
+	
+				# print info
+				fh.write('Analyzed %g station pairs of %g successfully.\n' %(cnt_success,n))
+				fh.write('No data found for %g station pairs.\n' %cnt_unavail)
+				fh.write('No measurement taken for %g station pairs due to short interstation distance.\n' %cnt_overlap) 
+				fh.write('Signal to noise ratio below threshold for %g station pairs.\n' %cnt_lowsnr)
+				fh.write('Number of staacked windows below threshold for %g station pairs.\n' %cnt_lown)
+				fh.write('\nParameters:==============================================================\n')
+				fh.write('Source dir: %s \n' %source_model)
+				fh.write('Step: %g' %int(step))
+				fh.write('Minimum SNR: %g' %snr_min)
+				fh.write('Minimum stack length: %g' %int(n_min))
+				fh.write('Save all interstation gradients: %s' %str(save_all))
+				fh.write('\n=========================================================================\n')
+				fh.write('Project:\n')
+				# append configurations
+				cfg = open(os.path.join(source_config['project_path'],'config.json')).read()
+				fh.write(cfg)
+				fh.write('\n=========================================================================\n')
+				fh.write('Source model:\n')
+				fh.write(json.dumps(source_config))
+				fh.write('\n=========================================================================\n')
+				fh.write('Measurement:\n')
+				cfg = open(os.path.join(source_config['source_path'],'measr_config.json')).read()
+				fh.write(cfg)
+			
+	
+			
+		# co	llect
+			gradient[ix_basis,:] += kernel * var_weights[ix_basis]
+			del kernel
 
 # save
 	if save_all:
