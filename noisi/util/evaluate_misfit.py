df87847342cfb8a676b78c4ffe6902e7db749449
lermert <laura.ermert@erdw.ethz.ch>
2017-11-15 12:11:37 +0100

    misfit evaluation for step length test

---
+++ noisi/util/evaluate_misfit.py
@@ -0,0 +1,38 @@
+import sys
+from pandas import read_csv
+import json
+import os
+from glob import glob
+
+# evaluate measurement
+source_dir = sys.argv[1]
+step = sys.argv[2]
+
+# get weights for freq. bands
+measr_config = json.load(open(os.path.join(source_dir,
+	'measr_config.json')))
+weights = measr_config['weights']
+mtype = measr_config['mtype']
+
+# for each freq. band,...
+step_tests = glob(os.path.join(source_dir,'step_'+step,'steptest_*'))
+
+result = np.zeros((len(step_tests),2))
+
+for i in range(len(step_tests)):
+	for j in range(len(weights)):
+		# what step length?
+		result[i,0](float(step_tests.split('_')[-1]))
+
+		# load the measurements
+		filename = '{}.{}.measurement.csv'.format(mtype,j)
+		msr_file = os.path.join(step_tests[i],filename)
+		dat = read_csv(msr_file)
+
+	
+		# take the average
+		result[i,1] += dat.l2_norm.mean()/len(weights) * weights[j]
+
+
+np.save('evaluation_step_length_test.npy',result)
+
