60e6df202c47248c31ee07ac2f527862e999f73d
lermert <laura.ermert@posteo.net>
2018-04-27 16:39:50 +0100

    upps 2.

--- noisi/main.py
+++ noisi/main.py
@@ -32,7 +32,8 @@ def setup_project(project_name):
     if os.path.exists(project_name):
         click.echo('Project exists already, must give it a new name.')
         exit()   
-    # #os.makedirs(os.path.join(project_name,'observed_correlations'))    
+    else:
+        os.makedirs(os.path.join(project_name))    
     from . import _ROOT
     with io.open(os.path.join(_ROOT,'config','config.json'),'r+') as fh:
         conf = json.loads(fh.read())
