19597ae7ce9f172f4562f4cb7409943c1bc4d91e
Jonas Igel <jigel@student.ethz.ch>
2018-06-11 15:57:53 +0100

    Gauss main & setup update
    Files update to work with Laura's new file structure

--- noisi/main.py
+++ noisi/main.py
@@ -36,38 +36,7 @@ def setup_project(project_name):
         exit()
     else:
         setup_proj(project_name)
-        os.makedirs(os.path.join(project_name))
 
-    from . import _ROOT
-    with io.open(os.path.join(_ROOT,'config','config.json'),'r+') as fh:
-        conf = json.loads(fh.read())
-    
-    # For gaussian grid
-    with io.open(os.path.join(_ROOT,'config','config_gauss.json'),'r+') as fh:
-        conf_gauss = json.loads(fh.read())
-        
-    conf['date_created'] = time.strftime("%Y.%m.%d")
-    conf['project_name'] = project_name
-    conf['project_path'] = os.path.abspath(project_name)
-    
-    conf_gauss['date_created'] = time.strftime("%Y.%m.%d")
-    conf_gauss['project_name'] = project_name
-    conf_gauss['project_path'] = os.path.abspath(project_name)
-    
-    with io.open(os.path.join(project_name,'config.json'),'w') as fh:
-        cf = json.dumps(conf,sort_keys=True, indent=4, separators=(",", ": "))
-        fh.write(cf)
-
-    # For gaussian grid
-    with io.open(os.path.join(project_name,'config_gauss.json'),'w') as fh:
-        cf = json.dumps(conf_gauss,sort_keys=True, indent=4, separators=(",", ": "))
-        fh.write(cf)
-    
-    # Copy gaussian grid notebook
-    from . import _ROOT
-    os.system('cp {} {}'.format(os.path.join(_ROOT,'jnotebks/setup_gaussian_grid.ipynb'),
-    project_name))
-    
     click.secho("Copied default config.json and config_gauss.json to project directory, please edit.\
                 Use setup_gaussian_grid.ipynb to visually setup gaussian grid.")
 
--- noisi/util/setup_new.py
+++ noisi/util/setup_new.py
@@ -10,11 +10,28 @@ def setup_proj(project_name):
     
     with io.open(os.path.join(_ROOT,'config','config.json'),'r+') as fh:
         conf = json.loads(fh.read())
+    
+    # For gaussian grid
+    with io.open(os.path.join(_ROOT,'config','config_gauss.json'),'r+') as fh:
+        conf_gauss = json.loads(fh.read())
         
     conf['date_created'] = time.strftime("%Y.%m.%d")
     conf['project_name'] = project_name
     conf['project_path'] = os.path.abspath(project_name)
     
+    conf_gauss['date_created'] = time.strftime("%Y.%m.%d")
+    conf_gauss['project_name'] = project_name
+    conf_gauss['project_path'] = os.path.abspath(project_name)
+    
     with io.open(os.path.join(project_name,'config.json'),'w') as fh:
         cf = json.dumps(conf,sort_keys=True, indent=4, separators=(",", ": "))
         fh.write(cf)
+        
+    # For gaussian grid
+    with io.open(os.path.join(project_name,'config_gauss.json'),'w') as fh:
+        cf = json.dumps(conf_gauss,sort_keys=True, indent=4, separators=(",", ": "))
+        fh.write(cf)
+        
+    # Copy gaussian grid notebook
+    os.system('cp {} {}'.format(os.path.join(_ROOT,'jnotebks/setup_gaussian_grid.ipynb'),
+    project_name))
