993f63a9a5145a48d5c56704932864fb8b015f36
Jonas Igel <jigel@student.ethz.ch>
2018-06-15 15:56:54 +0100

    Undo source_grid
    Undo changes to the source_grid due to ZeroDivision

--- noisi/scripts/source_grid.py
+++ noisi/scripts/source_grid.py
@@ -31,13 +31,9 @@ radius=None):
     
     while lat <= ymax:
         d_lat = dx / len_deg_lat(lat)
-        
-        if abs(lat) == 90:
-        # length of a degree longitude will be 0.
-            lon = xmin
-        else:
-            d_lon = dx / len_deg_lon(lat)
-            lon = xmin + np.random.rand(1)[0] * d_lon
+        d_lon = dx / len_deg_lon(lat)
+            
+        lon = xmin + np.random.rand(1)[0] * d_lon
 
         while lon <= xmax:
                     
