e15ee3765c6777daed60888f894d3a8b82f09ad5
lermert <laura.ermert@posteo.net>
2018-07-04 14:40:36 +0100

    more relaxed tests yet

--- noisi/test/test_process.py
+++ noisi/test/test_process.py
@@ -8,7 +8,8 @@ def test_process():
 	f1 = h5py.File('test/testdata/testsrc/wavefield_processed/NET.STA1..CHA.h5')
 	f2 = h5py.File('test/testdata/testsrc/wavefield_processed_archived/NET.STA1..CHA.h5')
 
-	assert (f1['data'][:][0]-f2['data'][:][0]).sum() < 1.e-16
+	assert ((f1['data'][:][0]-f2['data'][:][0])/
+		f1['data'][:][0]).max() < 1.e-6
 	
 	f1 = h5py.File('test/testdata/testsrc/wavefield_processed/NET.STA2..CHA.h5')
 	f2 = h5py.File('test/testdata/testsrc/wavefield_processed_archived/NET.STA2..CHA.h5')
